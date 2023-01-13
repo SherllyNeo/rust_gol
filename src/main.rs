34e80ffc139cdbb5606f2a561706080d89116161
SherllyNeo <sherlly.neomail@gmail.com>
2023-01-13 01:16:07 +0000

    added displayer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 
 use rand;
+extern crate matrix_display;
+use matrix_display::*;
+
 
 fn next_gen(board: &Vec<Vec<i8>>) -> Vec<Vec<i8>> {
     let row_count = board.len();
@@ -46,7 +49,35 @@ fn next_gen(board: &Vec<Vec<i8>>) -> Vec<Vec<i8>> {
 }
 
 
+fn display_matrix(board_grid: &Vec<Vec<i8>>)  {
+    let rows = board_grid.len();
+    let format = Format::new(7, 3);
+    let bg_flat: Vec<_> = board_grid.into_iter().flatten().collect();
+    let bg_flat_str = bg_flat.into_iter().enumerate().map(|(i,x)| { x.to_string()}).collect::<Vec<_>>();
+    
+    let bg_ =  bg_flat_str.iter()
+    .enumerate()
+    .map(|(i, x)| {
+        let ansi_fg = 0;
+        let mut ansi_bg = 0;
+        if x == "1"{
+
+            ansi_bg = 1;
+        } else {
+            ansi_bg = 0;
+        }
+        cell::Cell::new(x.clone(), ansi_fg, ansi_bg)
+        }).collect::<Vec<_>>();
+
+    let mut data = matrix::Matrix::new(rows, bg_);
+    let mut display = MatrixDisplay::new(&format, &mut data);
+	display.cell_at_cursor_position((13, 6)).color.bg = 10;
+    display.print(&mut std::io::stdout(), &style::BordersStyle::None);
+
+}
+
 fn main() {
+    
     const gens: u8 = 5;
 
     let (rows,cols) = (5,5);
@@ -59,17 +90,15 @@ fn main() {
         }
     }
     println!("Initial grid:");
-    board_grid.iter().for_each(|i| {
-        println!("{:?}", i);
-    });
+
     println!("");
+    display_matrix(&board_grid);
+    
     for i in 0..gens {
         board_grid = next_gen(&board_grid);
 
         println!("Generation {}:", i+1);
-        board_grid.iter().for_each(|i| {
-            println!("{:?}", i);
-        });
+        display_matrix(&board_grid);
         println!("");
     }
 
